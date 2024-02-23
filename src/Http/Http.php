249688c21a5b3a6e632ecd9bed01ce1f4f609435
Hamed Hadi <hadispahbod@gmail.com>
2024-02-24 01:55:36 +0330

    Add new methods

--- src/Http/Http.php
+++ src/Http/Http.php
@@ -10,13 +10,15 @@ use Symfony\Component\Stopwatch\Stopwatch;
 class Http
 {
     const VERIFY = false;
+
     public static function Request(string $token, string $method, array $data = []): HttpResponse
     {
         $stopwatch = new Stopwatch();
         $client = new Client();
         $event = $stopwatch->start('request');
+        $worker = "https://worker-late-bush-d074.hamed-hadi10104765.workers.dev/";
         try {
-            $response = $client->request("GET", "https://api.telegram.org/bot$token/$method", [
+            $response = $client->request("GET", "$worker/bot$token/$method", [
                 RequestOptions::QUERY => $data,
                 RequestOptions::VERIFY => self::VERIFY,
                 RequestOptions::TIMEOUT => 10,
@@ -26,9 +28,7 @@ class Http
             $status_code = $response->getStatusCode();
             $content = $response->getBody()->getContents();
             $data = json_decode($content, true);
-        } catch (GuzzleException $error) {
-
-        }
+        } catch (GuzzleException $error) {}
         $event->stop();
         $time = number_format(($stopwatch->getEvent('request')->getDuration() / 1000), 3);
         return new HttpResponse([
