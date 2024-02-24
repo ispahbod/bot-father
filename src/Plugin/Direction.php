3bbaa6d544615d3b33012e45adf14704c1c21ef4
Hamed Hadi <hadispahbod@gmail.com>
2024-02-24 13:18:16 +0330

    Add new methods

--- src/Plugin/Direction.php
+++ src/Plugin/Direction.php
@@ -2,6 +2,8 @@
 
 namespace LaravelBot\BotFather\Plugin;
 
+use LaravelBot\BotFather\Helper\LanguageDirection;
+
 class Direction
 {
     private string $active_direction = self::LTR;
@@ -10,9 +12,14 @@ class Direction
     public const SHUFFLE = 'shuffle';
     public const SHUFFLE_ROW = 'shuffle_row';
     public const SHUFFLE_COLUMN = 'shuffle_column';
+
     public function DIR(): string
     {
         return $this->active_direction;
+    }
+    public static function Lang(string $languageCode): string
+    {
+        return LanguageDirection::Get($languageCode);
     }
     public function ActiveDirection(string $direction): self
     {
