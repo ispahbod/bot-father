7f53f7e12a5029507213aad226bb0ac5aec9493e
Hamed Hadi <hadispahbod@gmail.com>
2024-03-06 14:06:33 +0330

    Add BotCommand

---
+++ src/Plugin/BotCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Ispahbod\BotFather\Plugin;
+
+use Ispahbod\BotFather\Field\BotCommandField;
+
+final class BotCommand
+{
+    public static function Commands($array): string
+    {
+        return json_encode($array);
+    }
+
+    public static function Command(string $command, string $description = ''): array
+    {
+        return [
+            BotCommandField::COMMAND => $command,
+            BotCommandField::DESCRIPTION => $command,
+        ];
+    }
+}
