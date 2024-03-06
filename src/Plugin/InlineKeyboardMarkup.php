2c3b4de5d8c358a305faa9bd062ddb083ef4de32
Hamed Hadi <hadispahbod@gmail.com>
2024-03-06 19:50:30 +0330

    Update InlineKeyboardMarkup

--- src/Plugin/InlineKeyboardMarkup.php
+++ src/Plugin/InlineKeyboardMarkup.php
@@ -3,6 +3,7 @@
 namespace Ispahbod\BotFather\Plugin;
 
 use Ispahbod\BotFather\Constant\InlineKeyboardMarkupConfig;
+use Ispahbod\BotFather\Constant\ReplyKeyboardMarkupConfig;
 use Ispahbod\BotFather\Helper\ArrayDirManipulator;
 
 class InlineKeyboardMarkup
@@ -16,7 +17,30 @@ class InlineKeyboardMarkup
         }
         return json_encode(['inline_keyboard' => $array]);
     }
-
+    public static function CreateGrid($grids, $config = []): string
+    {
+        $dir = ReplyKeyboardMarkupConfig::DIRECTION;
+        if (isset($config[$dir])) {
+            ArrayDirManipulator::ManipulateArray($grids, $config, $dir);
+        }
+        $mergedArray = call_user_func_array('array_merge', $grids);
+        return json_encode(array_merge(['inline_keyboard' => $mergedArray], $config));
+    }
+    public static function Grid($array, int|array $orders = 1): array
+    {
+        $array = array_filter($array);
+        if (is_int($orders)) {
+            $result = self::Row(array_chunk($array, $orders));
+        } else {
+            $result = [];
+            $index = 0;
+            foreach ($orders as $order) {
+                $result[] = self::Row(array_slice($array, $index, $order));
+                $index += $order;
+            }
+        }
+        return $result;
+    }
     public static function Row(array $array,bool $exp = true): array
     {
         return $exp ? $array : [];
