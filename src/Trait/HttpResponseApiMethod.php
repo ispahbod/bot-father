027097f2ee81e8190fb0914645da8a39b1888436
Hamed Hadi <hadispahbod@gmail.com>
2024-05-30 18:26:58 +0330

    Update package

--- src/Trait/HttpResponseApiMethod.php
+++ src/Trait/HttpResponseApiMethod.php
@@ -8,11 +8,26 @@ trait HttpResponseApiMethod
 {
     private function httpResponseApiMethod($method, $data = []): array
     {
-        $http = Http::request($this->token, $method, $data);
-        $content = $http->getContent();
-        return array_merge(is_string($content) ? ['content' => $content] : $content, [
-            'error_code' => $http->getErrorCode(),
-            'description' => $http->getErrorMessage(),
-        ]);
+        try {
+            $http = Http::request($this->token, $method, $data);
+            if ($http->isSuccess()) {
+                $content = $http->getContent();
+                $response = is_string($content) ? ['content' => $content] : $content;
+            } else {
+                $response = [
+                    'error_code' => $http->getErrorCode(),
+                    'description' => $http->getErrorMessage(),
+                ];
+            }
+        } catch (\Exception $e) {
+            $response = [
+                'error_code' => 500,
+                'description' => 'Internal Server Error: ' . $e->getMessage(),
+            ];
+        }
+        if ($response === true || $response === false) {
+            $response = [];
+        }
+        return $response;
     }
 }
